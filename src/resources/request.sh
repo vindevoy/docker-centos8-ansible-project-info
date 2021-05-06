2a8de455f079f25e726b132471b5550fa51cc47f
Yves Vindevogel <yves.vindevogel@asynchrone.com>
2021-05-06 19:15:52 +0200

    Working version, without filtering output

--- src/resources/request.sh
+++ src/resources/request.sh
@@ -1,1 +1,21 @@
-echo "{ 'hello': 'world' }" > /var/www/openresty/request.json
+
+# ABOVE THIS LINE, THE NEEDED VARS ARE SET FROM THE DOCKERFILE
+# DO NOT TOUCH BELOW, AFTER DEVELOPMENT
+
+ANSIBLE_DIR=/var/opt/ansible
+OUTPUT_DIR=/var/www/openresty
+OUTPUT_FILE=request.json
+
+mkdir -p $OUTPUT_DIR
+touch $OUTPUT_DIR/$OUTPUT_FILE
+
+
+cd $ANSIBLE_DIR
+git checkout $GIT_BRANCH    
+git pull origin $GIT_BRANCH  
+
+echo Generating output: $OUTPUT_DIR/$OUTPUT_FILE
+
+ansible-inventory -i $HOSTS_FILE --list | sponge $OUTPUT_DIR/$OUTPUT_FILE
+
+echo "Request done"
